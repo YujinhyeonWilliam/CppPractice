7b54fc1adc8bb1b0f99bf560bcb8a5161b5b7c0b
Jinhyeon <williamjinhyeon@g1play.co.kr>
2023-11-01 18:00:49 +0900

    Update Second Lecture to Third one

--- one_jblecture/main.cpp
+++ one_jblecture/main.cpp
@@ -1,28 +1,15 @@
 #include <iostream>
 
-namespace A {
-
-	void function()
-	{
-		std::cout << "Fucntion 1" << std::endl;
-	}
-}
-
-namespace B
+void func()
 {
-	void function()
-	{
-		std::cout << "Function 2" << std::endl;
-	}
+	std::cout << "Hello";
 }
 
 int main()
 {
-	int age = 26;
-	float height = 175.2f;
-	std::cout << " my age is " << age << "\n";
-	std::cout << " my height is " << height;
+	int a = 10;
 
+	func();
 
-	return 0; // optional
+	int b = 30;
 }
