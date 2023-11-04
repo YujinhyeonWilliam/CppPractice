d807b7548776cb41e26c11d10a2e854a0a692172
JinhyeonYu <williamjinhyeon1@gmail.com>
2023-11-04 16:28:27 +0900

    Finished 6th week's lectures

--- one_jblecture/main.cpp
+++ one_jblecture/main.cpp
@@ -1,64 +1,133 @@
 #include <iostream>
-#include "Account.h"
 using namespace std;
 
-class GameManager
+//class Player
+//{
+//private:
+//	int x, y;
+//	int speed;
+//public:
+//	Player(int x, int y, int speed)
+//		: x {x}, y {y}, speed { speed }
+//	{}
+//
+//	void Move(int dx, int dy)
+//	{
+//		x += dx * speed;
+//		y += dy * speed;
+//	}
+//	
+//	void ShowPosition() const
+//	{
+//		cout << x << ", " << y << endl;
+//	}
+//};
+//
+//
+//class PlayerHandler
+//{
+//private:
+//	Player* playerList[50];
+//	int playerNum;
+//public:
+//	PlayerHandler() : playerNum{ 0 }{}
+//
+//	~PlayerHandler()
+//	{
+//		for (int i = 0; i < playerNum; i++)
+//			delete playerList[i];
+//	}
+//	
+//	void AddPlayer(Player* player)
+//	{
+//		playerList[playerNum++] = player;
+//	}
+//
+//	void ShowAllPlayerPositions() const
+//	{
+//		for (int i = 0; i < playerNum; i++)
+//			playerList[i]->ShowPosition();
+//	}
+//
+//	
+//};
+
+
+class Entity 
 {
+protected:
+	int _x;
+	int _y;
+
+public:
+	Entity(int x, int y)
+		: _x {x}, _y {y} {}
+
+	void ShowPosition()
+	{
+		cout << "[" << _x << ", " << _y << "]" << endl;
+	}
+
+	void Talk()
+	{
+		cout << "Hello" << endl;
+	}
+
+	Entity(const Entity& other)
+	{
+
+	}
 
 };
 
-class Player
+
+
+class Player : public Entity
 {
-	friend class GameManager;
-	friend void DisplayPlayer(const Player& p);
 private:
-	int x, y;
-	float speed;
-	static int numPlayer;
+	int* _hp;
+	int* _xp;
+	int* _speed;
 public:
 
-#pragma region Constructor and Destoryer
-
-	Player(int x, int y, float speed)
-		: x{ x }, y{ y }, speed{ speed }
-	{
-		cout << this << endl;
-		numPlayer++;
-	}
+	Player(int x, int y, int speed) : Entity{ x, y }, _speed{ new int } { *_speed = speed; }
 	~Player()
 	{
-		numPlayer--;
+		delete _hp;
+		_hp = nullptr;
+		
+		delete _xp;
+		_xp = nullptr;
+
+		delete _speed;
+		_speed = nullptr;
 	}
 
-#pragma endregion
-
-	void SetPosition(int x, int y) {
-		this->x = x;
-		this->y = y;
-	}
-
-	void PrintPosition() const
+	void Move(int dx, int dy)
 	{
-		cout << x << ", " << y << endl;
+		_x += dx * (*_speed);
+		_y += dy * (*_speed);
+	}
+	Player(const Player& player)
+		: Entity { player }, _hp { new int }, _xp { new int }, _speed { new int }
+	{
+		*_hp = *player._hp;
+		*_xp = *player._xp;
+		*_speed = *player._speed;
 	}
 
-	static int GetNumPlayer() { return numPlayer; }
-
+	void Talk()
+	{
+		Entity::Talk();
+		cout << "I'm player." << endl;
+	}
 };
-
-int Player::numPlayer = 0;
-
-void DisplayPlayer(const Player& p)
-{
-	cout << p.x << "," << p.y << endl; //friend가 아니면 private에 접근불가능
-}
 
 int main()
 {
-    Player p{ 1,1,1 };
-	Player p2{ 1,1,1 };
-	Player Monster { 1,1,1 };
-
-	std::cout << Player::GetNumPlayer() << std::endl;
-	DisplayPlayer(p);
+	Player player{ 5, 5, 10 };
+	player.ShowPosition();
+	player.Talk();
+	player.Move(1, 5);
+	player.ShowPosition();
 }
